58d23c737c798406d87cb5cd6f376759853430a8
imarcaos <marcaos.melo@gmail.com>
2022-10-31 16:23:59 +0000

    Practice Basic Operations - Nullability/Lists - ex. 4of5

---
+++ Nullabitlity_Lists/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Nullabitlity_Lists"
+
---
+++ Nullabitlity_Lists/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    /* 31/10/2022 - Practice Basic Operations - Nullability/Lists - ex. 4of5
+    Practice Time: Nullability/Lists
+    Create a list with two elements that are null; do it in two different ways.
+    Next, create a list where the list is null.
+     */
+
+    var list = listOf(null, null)
+    var list1: List<Int?> = listOf(null, null)
+
+    // Null list
+    var list2: List<Int>? = null
+
+}
