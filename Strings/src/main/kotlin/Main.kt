fb8b657a8938170c70ed2f09b367e2960adf041f
imarcaos <marcaos.melo@gmail.com>
2022-11-01 11:04:46 +0000

    Practice Time: Strings - e.g.1

---
+++ Strings/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    /* 01/11/2022
+    Practice Time: Strings - e.g.1
+    Create three String variables for trout, haddock, and snapper.
+    Use a String template to print whether you do or don't like to eat these kinds of fish.
+     */
+
+    var trout: String = "trout"
+    var haddok: String = "haddok"
+    var snapper: String = "snapper"
+
+    println("I like $trout fish, the $haddok fish and $snapper fish I never tried")
+}
