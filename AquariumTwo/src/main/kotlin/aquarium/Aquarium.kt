44063a331f446eb9eae09a6668c025939315cc7b
imarcaos <marcaos.melo@gmail.com>
2022-11-22 18:17:32 +0000

    Classes class ex.4-4 - first Constructors

--- AquariumTwo/src/main/kotlin/aquarium/Aquarium.kt
+++ AquariumTwo/src/main/kotlin/aquarium/Aquarium.kt
@@ -5,4 +5,9 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
     var volume : Int
         get() = width * height * length / 1000
         set(value) {height = (value * 1000) / (width * length)} // change height value
+
+    var water = volume * 0.9
+
+    // Here we need to declare the name constructor for Constructor
+    constructor(numberOfFish: Int): this() {}
 }
--- AquariumTwo/src/main/kotlin/aquarium/main.kt
+++ AquariumTwo/src/main/kotlin/aquarium/main.kt
@@ -22,4 +22,11 @@ fun buildAquarium() {
     //ex: 1
     val smallAquarium = Aquarium(length = 20, width = 15, height = 30)
     println("Small Aquarium: ${smallAquarium.volume} liters")
+
+    //ex: 2
+    val myAquarium2 = Aquarium(numberOfFish = 9)
+    println("Small Aquarium 2: ${myAquarium2.volume} liters with "
+            +"lenght ${myAquarium2.length}, "
+            +"width ${myAquarium2.width}, "
+            +"height ${myAquarium2.height}")
 }
